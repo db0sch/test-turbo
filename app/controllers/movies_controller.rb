93946662581b3166016c25c927332a03e45e7407
Dimitri Bosch <dimitribosch@hotmail.com>
2024-01-09 17:25:52 +0100

    Demo

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,10 +5,6 @@ class MoviesController < ApplicationController
     @movies = Movie.all
   end
 
-  def new
-    @movie = Movie.new
-  end
-
   def create
     @movie = Movie.new(movie_params)
     if @movie.save
